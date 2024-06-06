e6a9cba50a523f39a4b8629143de7d58dbb4afdf
Khenziii <khenzii.was.taken@gmail.com>
2024-06-06 15:28:08 +0200

    feat(nixos): switch to hyprland

--- configuration.nix
+++ configuration.nix
@@ -42,12 +42,13 @@
     LC_TIME = "pl_PL.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = false;
+  services.xserver.desktopManager.plasma5.enable = false;
+  programs.hyprland = {
+    enable = true;
+	xwayland.enable = true;
+  };
 
   # Configure keymap in X11
   services.xserver = {

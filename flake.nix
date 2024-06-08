48490b92be788677b0b907906fdbc971d0c9a6de
Khenziii <khenzii.was.taken@gmail.com>
2024-06-08 17:06:21 +0200

    feat(nixos): fix flake build
    Sadly, this forces us to use to `--impure` flag. It didn't seem to build
    without it.

--- flake.nix
+++ flake.nix
@@ -7,8 +7,7 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-       ./configuration.nix
-	   ./hardware-configuration.nix
+        /etc/nixos/configuration.nix
       ];
     };
   };

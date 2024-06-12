62bc9f07f37ab012f7d38d7fe9bd7268fd5b734a
Khenziii <khenzii.was.taken@gmail.com>
2024-06-12 23:30:35 +0200

    fix(nixos): remove duplicate code

--- home.nix
+++ home.nix
@@ -53,12 +53,6 @@
     })
   ];
 
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { 
-	  fonts = [ "Hack" ]; 
-    })
-  ];
-
   catppuccin.flavor = "mocha";
   catppuccin.accent = "mauve";
   catppuccin.enable = true;

d457183089d77c5fcac67ab66b6b0298263bbce4
Khenziii <khenzii.was.taken@gmail.com>
2024-06-12 23:33:13 +0200

    fix(nixos): fix copied code

--- home.nix
+++ home.nix
@@ -18,7 +18,7 @@
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git

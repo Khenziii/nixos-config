43af094a0053b490172efe5fad864c8c4f414452
Khenziii <khenzii.was.taken@gmail.com>
2024-06-16 18:13:57 +0200

    refactor(nixos): create `discord.nix`

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,7 +7,6 @@
   ];
 
   home.packages = with pkgs; [
-    discord
     spotify
     spicetify-cli
     hyprshot
--- home-manager/modules/packages/default.nix
+++ home-manager/modules/packages/default.nix
@@ -13,6 +13,7 @@
 	./swaynotificationcenter.nix
 	./pavucontrol.nix
 	./networkmanagerapplet.nix
+	./discord.nix
   ];
 }
 

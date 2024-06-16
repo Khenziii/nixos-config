4652bb8329d73be251f13ec71d7e9288522655c7
Khenziii <khenzii.was.taken@gmail.com>
2024-06-16 18:01:38 +0200

    refactor(nixos): create `wl-clipboard.nix`

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,7 +7,6 @@
   ];
 
   home.packages = with pkgs; [
-    wl-clipboard
     swww
     libnotify
     swaynotificationcenter
--- home-manager/modules/packages/default.nix
+++ home-manager/modules/packages/default.nix
@@ -7,6 +7,7 @@
 	./kitty.nix
 	./neofetch.nix
 	./rofi.nix
+	./wl-clipboard.nix
   ];
 }
 

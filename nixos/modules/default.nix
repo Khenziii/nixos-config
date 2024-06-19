a2c4684ea6bc1948a37c9811987cf712be0c89a4
Khenziii <khenzii.was.taken@gmail.com>
2024-06-20 00:12:10 +0200

    fix(nixos): add missing imports

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -5,10 +5,13 @@
     ./bluetooth.nix
 	./bootloader.nix
 	./catppuccin.nix
+	./core.nix
 	./desktop-environment.nix
 	./location.nix
 	./networking.nix
+	./printing.nix
 	./sound.nix
+    ./users.nix
 	./packages
   ]    
 }

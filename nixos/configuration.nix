31e75db9cd2194ed97006e2e851839a5d8bfe9e4
Khenziii <khenzii.was.taken@gmail.com>
2024-06-19 23:16:26 +0200

    refactor(nixos): use `desktop-environment.nix`

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,20 +10,6 @@
 	../shared/shared.nix
   ];
 
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = false;
-  programs.hyprland = {
-    enable = true;
-	xwayland.enable = true;
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
--- nixos/modules/desktop-environment.nix
+++ nixos/modules/desktop-environment.nix
@@ -1,6 +1,18 @@
 { ... }:
 
 {
-  
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = false;
+  programs.hyprland = {
+    enable = true;
+	xwayland.enable = true;
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 }
 

e1cd64153895b008793cc3b7dea98b0ea1da5614
Khenziii <khenzii.was.taken@gmail.com>
2024-06-19 23:15:19 +0200

    refactor(nixos): use `networking.nix`

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,16 +10,6 @@
 	../shared/shared.nix
   ];
 
-  networking.hostName = inputs.hostname; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ inputs, ... }:
 
 {
-  
+  networking.hostName = inputs.hostname;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
 }
 

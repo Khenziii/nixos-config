b9da334c772e6cb44d6508877aa7893c57a3d2bb
Khenziii <khenzii.was.taken@gmail.com>
2024-06-20 15:27:10 +0200

    feat(nixos): modify the `rebuild` alias

--- home-manager/modules/packages/shell/default.nix
+++ home-manager/modules/packages/shell/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: let
   shared-config = {
     aliases = {
-      rebuild = "rebuild-home && rebuild-system";
+      rebuild = "rebuild-system && rebuild-home";
       rebuild-home = "mv .git .git-old && home-manager switch --flake '.#khenzii'; mv .git-old .git";
       rebuild-system = "mv .git .git-old && sudo nixos-rebuild switch --flake '.#nixos'; mv .git-old .git";
 	  trewa = "tree";

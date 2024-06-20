1a8e13f05b6e822b192036a738ae233f5c6d5c34
Khenziii <khenzii.was.taken@gmail.com>
2024-06-20 15:31:18 +0200

    feat(nixos): create thefuck aliases

--- home-manager/modules/packages/shell/default.nix
+++ home-manager/modules/packages/shell/default.nix
@@ -5,6 +5,8 @@
       rebuild-home = "mv .git .git-old && home-manager switch --flake '.#khenzii'; mv .git-old .git";
       rebuild-system = "mv .git .git-old && sudo nixos-rebuild switch --flake '.#nixos'; mv .git-old .git";
 	  trewa = "tree";
+	  fuck = "thefuck";
+	  bruh = "thefuck";
 	};
   };
   

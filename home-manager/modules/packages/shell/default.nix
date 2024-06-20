2e8dae7a839983f9a5304901e892c51bd7992164
Khenziii <khenzii.was.taken@gmail.com>
2024-06-20 15:25:52 +0200

    feat(nixos): create `trewa` alias

--- home-manager/modules/packages/shell/default.nix
+++ home-manager/modules/packages/shell/default.nix
@@ -4,6 +4,7 @@
       rebuild = "rebuild-home && rebuild-system";
       rebuild-home = "mv .git .git-old && home-manager switch --flake '.#khenzii'; mv .git-old .git";
       rebuild-system = "mv .git .git-old && sudo nixos-rebuild switch --flake '.#nixos'; mv .git-old .git";
+	  trewa = "tree";
 	};
   };
   

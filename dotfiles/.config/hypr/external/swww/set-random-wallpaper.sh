e5cdf359491dced44b700296cdbd2bc94a3b0d52
Khenziii <khenzii.was.taken@gmail.com>
2024-06-21 19:13:05 +0200

    feat(swww): add new wallpaper

--- dotfiles/.config/hypr/external/swww/set-random-wallpaper.sh
+++ dotfiles/.config/hypr/external/swww/set-random-wallpaper.sh
@@ -18,5 +18,5 @@ mv $script_directory/current-wallpaper/* $script_directory/enabled-wallpapers
 mv $script_directory/enabled-wallpapers/$random_wallpaper $script_directory/current-wallpaper/
 
 # make swww use it
-swww img $script_directory/current-wallpaper/$random_wallpaper --fill-color=1e1e2e --no-resize
+swww img $script_directory/current-wallpaper/$random_wallpaper --fill-color=1e1e2e --resize=fit
 

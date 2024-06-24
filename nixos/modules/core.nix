e66c4ce2bb49586be77f9872b747fd02a956f1fd
Khenziii <khenzii.was.taken@gmail.com>
2024-06-24 21:32:51 +0200

    feat(nixos): add a 4GB swap file

--- nixos/modules/core.nix
+++ nixos/modules/core.nix
@@ -9,5 +9,11 @@
   # This probably shouldn't be changed. Please,
   # read the wiki before modifying this.
   system.stateVersion = "23.11";
+
+  # Create a 4GB swap file
+  swapDevices = [{
+    device = "/swapfile";
+    size = 1024 * 4; # Size is in MB
+  }];
 }
 

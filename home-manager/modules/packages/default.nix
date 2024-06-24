0cf3fb555b85a046cad97e84f33563fef340635f
Khenziii <khenzii.was.taken@gmail.com>
2024-06-24 19:59:28 +0200

    refactor(nixos): create `gui-apps` directory

--- home-manager/modules/packages/default.nix
+++ home-manager/modules/packages/default.nix
@@ -5,30 +5,15 @@
 	./shell
 	./development-environment
 	./cli-utilities
-	./kitty.nix
+	./gui-apps
 	./rofi.nix
 	./swww.nix
 	./swaynotificationcenter.nix
-	./pavucontrol.nix
-	./networkmanagerapplet.nix
-	# ./discord.nix | vesktop is being used instead. In case of
-	# any issues, re-enabling this is okay.
-	# ./spotify.nix | spicetify is being used instead.
 	./hyprshot.nix
-	./caprine-bin.nix
     ./eww.nix	
-    ./vesktop.nix	
-    ./obs-studio.nix
-    # ./steam.nix | steam is enabled system-wide, as it requires
-	# opening some ports in the firewall for certain features.
     ./hyprcursor.nix
     ./nerdfonts.nix
-    ./super-tux.nix
-    ./super-tux-kart.nix
     ./swaylock.nix
-    ./mpv.nix
-	./nwg-displays.nix
-	./firefox.nix
   ];
 }
 
---
+++ home-manager/modules/packages/gui-apps/default.nix
@@ -0,0 +1,23 @@
+{ ... }:
+
+{
+  imports = [
+    ./caprine-bin.nix
+	# ./discord.nix | vesktop is being used instead. In case of
+	# any issues, re-enabling this is okay.
+	./firefox.nix
+	./kitty.nix
+	./mpv.nix
+	./networkmanagerapplet.nix
+	./nwg-displays.nix
+	./obs-studio.nix
+	./pavucontrol.nix
+	# ./spotify.nix | spicetify is being used instead.
+	# ./steam.nix | steam is enabled system-wide, as it requires
+	# opening some ports in the firewall for certain features.
+	./super-tux-kart.nix
+	./super-tux.nix
+	./vesktop.nix
+  ];
+}
+

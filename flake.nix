cc6bc6114666d404eda766d7f9a1a7dadf774495
Khenziii <khenzii.was.taken@gmail.com>
2024-06-28 00:13:08 +0200

    feat(nixos): add missing field to firefox-addons
    `inputs.nixpkgs.follows = "nixpkgs"` is not necessary for the catppuccin
    flake, because it doesn't use nixpkgs.

--- flake.nix
+++ flake.nix
@@ -8,7 +8,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 	catppuccin.url = "github:catppuccin/nix";
-	spicetify-nix.url = "github:the-argus/spicetify-nix";
+	spicetify-nix = { 
+	  url = "github:the-argus/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+	};
 	firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";

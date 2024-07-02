15a0506d92d74b6a26c0a32f50055c7b0680eff2
Khenziii <khenzii.was.taken@gmail.com>
2024-07-02 12:48:29 +0200

    feat(nixos): temporarily disable spicetify

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -6,7 +6,9 @@
 	./systemd.nix
 	./catppuccin.nix
 	./graphical-user-interfaces.nix
-	./spicetify.nix
+	# ./spicetify.nix | after a recent update, I have started
+    # having issues with this flake. For now, I'm going to use
+    # the official client.
 	./packages
   ];
 }

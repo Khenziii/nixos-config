874c8d311625a7211177f57de2c7dce9784960b5
Khenziii <khenzii.was.taken@gmail.com>
2024-07-04 20:58:13 +0200

    feat(nixos): install `zsh-nix-shell` zsh plugin

--- home-manager/modules/packages/shell/zsh.nix
+++ home-manager/modules/packages/shell/zsh.nix
@@ -18,6 +18,16 @@
       size = 1000000;
       share = true;
     };
+    plugins = [{
+      name = "zsh-nix-shell";
+      file = "nix-shell.plugin.zsh";
+      src = pkgs.fetchFromGitHub {
+        owner = "chisui";
+        repo = "zsh-nix-shell";
+        rev = "v0.8.0";
+        sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+      };
+    }];
     initExtra = ''
       # Prompt
       PS1='%F{#cba6f7}%n%F{#b4befe}@%F{#cba6f7}%m %F{#b4befe}%~ %F{#a6e3a1}$ '

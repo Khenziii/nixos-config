135410b855c611cf7b86c1c8f0c3deb1119ff8a2
Khenziii <khenzii.was.taken@gmail.com>
2024-08-17 15:11:59 +0200

    feat(nvim): improve telescope searches

--- dotfiles/.config/nvim/lua/plugins/configs/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/configs/telescope.lua
@@ -1,5 +1,36 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
+-- telescope won't show any results from these files / directories.
+local ignored_files_and_dirs = {
+    "node_modules",
+    "dist",
+    ".git",
+    ".next",
+    "yarn.lock",
+};
+
+-- telescope expects ignored files in this format:
+-- "!{first_ignored_file,second_ignored_file}"
+-- this functions handles the conversion of a string table
+-- to a single string in the mentioned format.
+local function string_table_to_glob_string(string_table)
+    local result = "!{";
+
+    for index, str in ipairs(string_table) do
+        -- concat our string to the result
+        result = result .. str;
+
+        -- if not the last item, concat a comma
+        if index < #string_table then
+            result = result .. ",";
+        end
+    end
+
+    -- close the string
+    result = result .. "}";
+    return result;
+end
+
 local function setup()
 	require("telescope").setup({
 		defaults = {
@@ -13,6 +44,8 @@ local function setup()
       			'--smart-case',
       			'--hidden', -- include dotfiles
       			'--no-ignore', -- include VCS ignored files 
+                '--glob', -- ignore files listed in `ignored_files_and_dirs`
+                string_table_to_glob_string(ignored_files_and_dirs),
     		},
 		},
 	})

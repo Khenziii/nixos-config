2fd6b5dee43d0e060aac7c5bda820a64104b92af
Khenziii <khenzii.was.taken@gmail.com>
2024-08-26 18:36:43 +0200

    fix(nixos): make `mpv` play audio
    https://askubuntu.com/questions/1412462/mpv-accidently-got-no-sound-problem

--- home-manager/modules/packages/gui-apps/mpv.nix
+++ home-manager/modules/packages/gui-apps/mpv.nix
@@ -1,13 +1,13 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  home.packages = with pkgs; [
-    mpv
-  ];
-
   programs.mpv = {
     enable = true;
 	catppuccin.enable = true; 
+    config = {
+      ao = "alsa";
+      audio-device = "auto";
+    };
   };
 }
 

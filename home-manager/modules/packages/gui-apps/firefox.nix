c7ade6da957c830f31f6f95f18829a4b0ab4453c
Khenziii <khenzii.was.taken@gmail.com>
2024-09-10 19:51:13 +0200

    feat(nixos): install `darkreader` firefox extension

--- home-manager/modules/packages/gui-apps/firefox.nix
+++ home-manager/modules/packages/gui-apps/firefox.nix
@@ -6,6 +6,8 @@
 	profiles.khenzii = {
 	  name = "Khenzii";
 	  isDefault = true;
+      # a list of extensions can be found here:
+      # https://gitlab.com/rycee/nur-expressions/-/blob/master/pkgs/firefox-addons/addons.json
       extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
         bitwarden
 		duckduckgo-privacy-essentials
@@ -14,6 +16,7 @@
 		firefox-color
 		violentmonkey
 		react-devtools
+        darkreader
 	  ];
 	  settings = {
 	    "extensions.autoDisableScopes" = 0; # auto enable extensions

d01871b9120e9dc642f8042dfbb8c1ee96cfb50d
Khenziii <khenzii.was.taken@gmail.com>
2025-02-22 16:01:43 +0100

    feat(nvim): version lock LSPs

--- dotfiles/.config/nvim/lua/plugins/configs/mason-lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/configs/mason-lspconfig.lua
@@ -3,18 +3,20 @@
 local function setup()
     require("mason-lspconfig").setup({
         ensure_installed = {
-            "lua_ls",
-            "tsserver",
-            "nil_ls",
-            "pylsp",
-            "csharp_ls",
-            "html",
-            "cssls",
-            "asm_lsp",
+            "lua_ls@3.10.6",
+            "tsserver@4.3.3",
+            "nil_ls@2024-08-06",
+            "pylsp@1.12.0",
+            "csharp_ls@0.15.0",
+            "html@4.10.0",
+            "cssls@4.10.0",
+            "asm_lsp@0.9.0",
+            -- `jdtls` doesn't support version locking..
+            -- TODO: switch to a better Java LSP
             "jdtls",
-            "kotlin_language_server",
-            "eslint",
-            "stylelint_lsp",
+            "kotlin_language_server@1.3.12",
+            "eslint@4.10.0",
+            "stylelint_lsp@2.0.0",
         },
     })
 end

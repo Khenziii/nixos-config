e734cd40ab8a1f72112c040b5da084640d1a6ff6
Khenziii <khenzii.was.taken@gmail.com>
2025-09-01 15:24:23 +0200

    chore(nvim): update LSP's name

--- dotfiles/.config/nvim/lua/plugins/configs/mason-lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/configs/mason-lspconfig.lua
@@ -4,7 +4,7 @@ local function setup()
     require("mason-lspconfig").setup({
         ensure_installed = {
             "lua_ls@3.10.6",
-            "tsserver@4.3.3",
+            "ts_ls@4.3.3",
             "nil_ls@2024-08-06",
             "pylsp@1.12.0",
             "csharp_ls@0.15.0",

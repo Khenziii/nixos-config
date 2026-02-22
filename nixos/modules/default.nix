9a5f9883afcd994f306f5e792f40ac2a362465ff
Krysteq <43525126+justkrysteq@users.noreply.github.com>
2026-02-22 23:02:13 +0100

    feat(nixos): use superior virtualization software (#20)

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,20 +1,21 @@
 { ... }:
 
 {
-  imports = [
-    ./bluetooth.nix
-	./boot.nix
-	./catppuccin.nix
-	./core.nix
-	./desktop-environment.nix
-	./location.nix
-	./networking.nix
-	./printing.nix
-	./sound.nix
-    ./users.nix
-    ./shell.nix
-    ./external.nix
-	./packages
-  ];
+	imports = [
+		./bluetooth.nix
+		./boot.nix
+		./catppuccin.nix
+		./core.nix
+		./desktop-environment.nix
+		./location.nix
+		./networking.nix
+		./printing.nix
+		./sound.nix
+		./users.nix
+		./shell.nix
+		./external.nix
+		./virtualisation.nix
+		./packages
+	];
 }
 
---
+++ nixos/modules/packages/gui-apps/virt-manager.nix
@@ -0,0 +1,5 @@
+{ ... }:
+
+{
+	programs.virt-manager.enable = true;
+}

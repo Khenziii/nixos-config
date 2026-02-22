0d09818b21fdee790377ab2957b61bbc22182945
Krysteq <43525126+justkrysteq@users.noreply.github.com>
2026-02-22 21:26:55 +0100

    feat(nixos): move git config and add ssh agent (#19)

--- nixos/modules/packages/cli-utilities/default.nix
+++ nixos/modules/packages/cli-utilities/default.nix
@@ -1,13 +1,19 @@
-{ ... }:
-
+{ lib, ... }:
+let
+	scanPaths = path:
+		map(f: (path + "/${f}"))
+		(builtins.attrNames
+			(lib.attrsets.filterAttrs
+				(
+					name: _type:
+						(_type == "directory" && builtins.pathExists (path + "/${name}/default.nix")) # include directories
+						|| (
+							(name != "default.nix") # ignore default.nix
+							&& (lib.strings.hasSuffix ".nix" name) # include .nix files
+						)
+				)
+				(builtins.readDir path)));
+in
 {
-  imports = [
-    ./git.nix
-	./home-manager.nix
-	./stow.nix
-	./vim.nix
-	./docker.nix
-	./exfatprogs.nix
-  ];
+	imports = scanPaths ./.; # Import all modules in the directory
 }
-

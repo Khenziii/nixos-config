0b11f8897957dfa5fc19ea2a105bfe4666749de1
Khenzii <khenzii@khenzii.dev>
2026-04-16 17:18:08 +0200

    feat(nixos): configure firewall to allow hosting LAN games through VPNs such as Hamachi

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -10,9 +10,15 @@
 	# Enable networking
 	networking.networkmanager.enable = true;
 
-	networking.firewall.allowedTCPPorts = [8080 8000 3979];
-	networking.firewall.allowedUDPPorts = [
-		34197 # Factorio.
-		3979 # OpenTTD.
-	];
+	networking.firewall = {
+		enable = true;
+		allowedTCPPorts = [8080 8000 3979 25565];
+		allowedUDPPorts = [
+			34197 # Factorio.
+			3979 # OpenTTD.
+			25565 # Minecraft.
+		];
+		trustedInterfaces = [ "ham0" ]; # TODO: do I need this???
+		checkReversePath = "loose";
+	};
 }

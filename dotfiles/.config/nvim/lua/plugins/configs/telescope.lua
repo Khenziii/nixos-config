fbb9a9d81d0923041e6bd608cf8661399af1ad19
Khenzii <khenzii@khenzii.dev>
2026-05-06 17:24:19 +0200

    feat(nvim): don't show search results from `__pycache__`

--- dotfiles/.config/nvim/lua/plugins/configs/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/configs/telescope.lua
@@ -2,18 +2,19 @@
 
 -- telescope won't show any results from these files / directories.
 local ignored_files_and_dirs = {
-    "node_modules",
-    "dist",
-    ".git",
-    ".next",
-    "yarn.lock",
-    "coverage",
-    ".nx",
-    ".idea",
-    "target",
-    "Cargo.lock",
-    "*.log",
-    "env",
+	"node_modules",
+	"dist",
+	".git",
+	".next",
+	"yarn.lock",
+	"coverage",
+	".nx",
+	".idea",
+	"target",
+	"Cargo.lock",
+	"*.log",
+	"env",
+	"__pycache__",
 };
 
 -- telescope expects ignored files in this format:
@@ -21,45 +22,46 @@ local ignored_files_and_dirs = {
 -- this function handles the conversion of a string table
 -- to a single string in the mentioned format.
 local function string_table_to_glob_string(string_table)
-    local result = "!{";
+	local result = "!{";
 
-    for index, str in ipairs(string_table) do
-        -- concat our string to the result
-        result = result .. str;
+	for index, str in ipairs(string_table) do
+		-- concat our string to the result
+		result = result .. str;
 
-        -- if not the last item, concat a comma
-        if index < #string_table then
-            result = result .. ",";
-        end
-    end
+		-- if not the last item, concat a comma
+		if index < #string_table then
+			result = result .. ",";
+		end
+	end
 
-    -- close the string
-    result = result .. "}";
-    return result;
+	-- close the string
+	result = result .. "}";
+	return result;
 end
 
 local function setup()
 	require("telescope").setup({
 		defaults = {
 			vimgrep_arguments = {
-      			'rg',
-      			'--color=never',
-      			'--no-heading',
-      			'--with-filename',
-      			'--line-number',
-      			'--column',
-      			'--smart-case',
-      			'--hidden', -- include dotfiles
-      			'--no-ignore', -- include VCS ignored files 
-                '--glob', -- ignore files listed in `ignored_files_and_dirs`
-                string_table_to_glob_string(ignored_files_and_dirs),
-    		},
+				'rg',
+				'--color=never',
+				'--no-heading',
+				'--with-filename',
+				'--line-number',
+				'--column',
+				'--smart-case',
+				'--hidden', -- include dotfiles
+				'--no-ignore', -- include VCS ignored files 
+				'--glob', -- ignore files listed in `ignored_files_and_dirs`
+				string_table_to_glob_string(ignored_files_and_dirs),
+			},
+			file_ignore_patterns = ignored_files_and_dirs,
 		},
 	})
 end
 
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 	config = setup,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
